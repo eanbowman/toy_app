24638ad32df009e90eecc3e7ebf2ef0ef330349e
Lilithe Lotor <lilithelotor@gmail.com>
2017-04-30 21:48:41 +0000

    Fixed controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+  	random_string = ('a'..'z').to_a.shuffle[0..7].join
+  	output = "¡Hola, mundo!" + "\n<br>" + random_string
+	render html: output.html_safe
+  end
+
+  def goodbye
+  	render html: "Goodbye"
+  end
 end
