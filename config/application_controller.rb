f6dff0693d0c700d6fe116168c8ca5b5271123a1
Lilithe Lotor <lilithelotor@gmail.com>
2017-04-30 20:24:36 +0000

    Fix?

--- config/application_controller.rb
+++ config/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+  	random_string = ('a'..'z').to_a.shuffle[0..7].join
+  	output = "¡Hola, mundo!" + "\n<br>" + random_string
+	render html: output.html_safe
+  end
+
+  def goodbye
+  	render html: "Goodbye"
   end
 end
